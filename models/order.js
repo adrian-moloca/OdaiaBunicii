22739400c1f573181eb2555bd28231bd148b8928
Moloca Adrian <adrian.moloca@smartboxdigital.com>
2021-12-22 07:43:27 +0200

    Update order.js

--- models/order.js
+++ models/order.js
@@ -6,7 +6,7 @@ const { Schema, model } = pkg;
 
 const orderSchema = new Schema({
     clientID: {type: String},
-    myOrder: [{b64Image: {type: String}, frame: {type: frameSchema}, numberOfItems: {type: Number, default: 1}}],
+    myOrder: [{b64Image: {type: String}, label: {type: String, default: ''}, source: {type: Number, default: 0}, type: {type: String, default: ''}, dim: {type: String, default: ''}, price: {type: String, default: ''}, numberOfItems: {type: Number, default: 1}}],
     contactDetails: {type: String},
 });
 orderSchema.plugin(uniqueValidator);
